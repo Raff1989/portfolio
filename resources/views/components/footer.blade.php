7953fc8e8b4c0ced2b1849dd613e7934023aa40c
Raffaele Ungolo <raffa_ele1989@libero.it>
2023-09-29 17:38:19 +0200

    work in progress

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,104 @@
+<!-- Footer -->
+<footer class="bg-dark text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4">
+  
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Facebook -->
+        <a class="btn btn1 btn-primary btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+  
+        <!-- Twitter -->
+        <a class="btn btn2 btn-primary btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+  
+        <!-- Google -->
+        <a class="btn btn3 btn-primary btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+  
+        <!-- Instagram -->
+        <a class="btn btn4 btn-primary btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+  
+        <!-- Linkedin -->
+        <a class="btn btn5 btn-primary btn-floating m-1" href="" role="button"><i class="fab fa-linkedin-in"></i></a>
+        <!-- Github -->
+        <a class="btn btn6 btn-primary btn-floating m-1" href="https://github.com/Raff1989" role="button"><i class="fab fa-github"></i></a>
+      </section>
+      <!-- Section: Social media -->
+      <!-- Section: Links -->
+      <section class="">
+        <!--Grid row-->
+        <div class="row justify-content-center">
+        <!--Grid column-->
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h4 class="text-uppercase">Products</h4>
+  
+            <ul class="list-unstyled mb-0">
+              <li>
+                <i class="text-white"><i class="fa-brands fa-html5"> Html5</i></i>
+              </li>
+              <li>
+                <i class="text-white"><i class="fa-brands fa-css3-alt"> Css3</i></i>
+              </li>
+              <li>
+                <i class="text-white"><i class="fa-brands fa-square-js"> Javascript</i></i>
+              </li>
+              <li>
+                <i class="text-white"><i class="fa-brands fa-php"> Php</i></i>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+  
+          <!--Grid column-->
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h4 class="text-uppercase">Framework</h4>
+  
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white"><i class="fa-brands fa-laravel"> Laravel</i></a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+          <!--Grid column-->
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h4 class="text-uppercase">Database</h4>
+  
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white"><i class="fa-solid fa-database"> Mysql</i></a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+          <!--Grid column-->
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h4 class="text-uppercase">Contatti</h4>
+  
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white"><i class="fa-solid fa-city"> Bisceglie (Bt)</i></a>
+              </li>
+              <li>
+                <a href="#!" class="text-white"><i class="fa-solid fa-envelope"> raffa_ele1989@libero.it</i></a>
+              </li>
+              <li>
+                <a href="#!" class="text-white"></li><i class="fa-solid fa-phone"> 3408383260</i></a>
+              
+            </ul>
+          </div>
+          <!--Grid column-->
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+  
+    </div>
+    <!-- Grid container -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2023 Copyright
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->
