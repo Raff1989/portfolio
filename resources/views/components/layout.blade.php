dd208510647968f6a5295bcbc0fba56347abc118
Raffaele Ungolo <raffa_ele1989@libero.it>
2023-09-30 12:51:00 +0200

    work-in-progress

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +9,11 @@
 </head>
 <body>
     <x-navbar />
-    {{$slot}}
+    
+        {{$slot}}
+    
+    
+    
     <x-footer />
     <script src="https://kit.fontawesome.com/f65e26a568.js" crossorigin="anonymous"></script>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,22 @@
 <x-layout>
-
-
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6">
+                <img class="parent" src="/images/foto per github.jpg" alt="photo">
+            </div>
+            <div class="col-6">
+                <h1 class="display-1 fw-bold shadow"> 𝓙unior <br> 𝓦eb <br> 𝓓eveloper </h1>
+            </div>
+        </div>
+    </div>
+    <div class="description">
+        <em class="display-3 text-center shadow fw-bold"> 𝓛a 𝓒reatività è 𝓒ontagiosa. <br> 𝓣rasmettila.
+            (𝓐lbert 𝓔instein)</em>
+    </div>
     
+
+
+
+
+
 </x-layout>
